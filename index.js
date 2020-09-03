f7103f86178c034e7b67c70fa1b5004743f50baa
Derrick Burns <derrick@tidepool.org>
2020-09-03 15:23:34 -0700

    Pass in user service via env

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var log = require('./lib/log.js')('index.js');
   var lifecycle = amoeba.lifecycle();
 
   var getter = {
-    get: function() { return [ "http://shoreline:9107" ] }
+    get: function() { return [ env.userApi.userService ] }
   }
 
   var userApiClient = require('user-api-client').client( config.userApi, getter );
