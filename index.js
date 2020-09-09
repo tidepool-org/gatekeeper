71f70798e5cd5f7a4dc173dd964b17da0bac3b8f
Derrick Burns <derrick@tidepool.org>
2020-09-08 22:39:42 -0700

    fix token validation

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var log = require('./lib/log.js')('index.js');
   var lifecycle = amoeba.lifecycle();
 
   var getter = {
-    get: function() { return [ config.userApi.userService ] }
+    get: function() { return [ new URL(config.userApi.userService) ] }
   }
 
   var userApiClient = require('user-api-client').client( config.userApi, getter );
